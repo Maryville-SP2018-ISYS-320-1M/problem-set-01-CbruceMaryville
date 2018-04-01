a79418663482571e8d3d715247fdbcfb7c8dc0e2
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-01 15:38:41 -0500

    Update P7_NestedLoopBox.java

--- src/P7_NestedLoopBox.java
+++ src/P7_NestedLoopBox.java
@@ -1,18 +1,25 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s):Clayton Bruce
+  	Date: 4/1/2018
 */
 
 /* 4. Your pseudocode algorithm here
-
+ 2 for loops same ints via 6 with nested stars
 
 */
 public class P7_NestedLoopBox {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		
+		for(int i = 0; i<6; i++) {
+			System.out.println();
+			for(int j =0;j<6; j++) {
+				System.out.print("*");
+			}
+		}
 	}
 
 }
