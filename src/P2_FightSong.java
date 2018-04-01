a5650ca7bb897a0986019870951206d94d460ef4
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-01 15:21:56 -0500

    Update P2_FightSong.java

--- src/P2_FightSong.java
+++ src/P2_FightSong.java
@@ -1,11 +1,35 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s):Clayton Bruce
+  	Date: 4/1/2018
 */
 
 public class P2_FightSong {
 	public static void main(String[] args) {
+		Intro();
+		Repeat();
+		Repeat();
+		Intro();
+	}
 
+	public static void Intro() {
+		System.out.println("Go,team, go!");
+		System.out.println("You can do it");
+		System.out.println();
+		
+
+	}
+
+	
+
+	public static void Repeat() {
+		System.out.println("Go,team, go!");
+		System.out.println("You can do it");
+		System.out.println("Your're the best,");
+		System.out.println("In the West.");
+		System.out.println("Go,team, go!");
+		System.out.println("You can do it");
+		System.out.println();
+	
 	}
 }
