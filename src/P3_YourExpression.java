b74e7f4fe56f8876bb1bb183610af5addbd945e4
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-01 15:25:44 -0500

    Update P3_YourExpression.java

--- src/P3_YourExpression.java
+++ src/P3_YourExpression.java
@@ -1,12 +1,31 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s):Clayton Bruce
+  	Date: 4/1/2018
 */
 public class P3_YourExpression {
 
 	public static void main(String[] args) {
-
+		TwentyFour();
 	}
 
+	
+	
+	public static void TwentyFour() {
+		
+		System.out.println((30/2)+10-1);
+	
+	}
 }
+
+
+/*
+
+	three operators
+	one pair of parentheses 
+	must = 24
+	
+	(30/2)+10-1 =24
+	
+
+*/
