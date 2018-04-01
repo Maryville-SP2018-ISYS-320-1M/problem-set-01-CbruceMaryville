6feecc880437a2c814d0d7467c40ed1855f68d71
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-01 15:30:49 -0500

    Update P4_Position.java

--- src/P4_Position.java
+++ src/P4_Position.java
@@ -1,12 +1,21 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s):Clayton Bruce
+  	Date: 4/1/2018
 */
 
 public class P4_Position {
 
 	public static void main(String[] args) {
+		Phys(2,3,4,5);
+	}
+
+	public static void Phys(int s0, int v0t, int a, int t) {
+
+	
+		double s = (s0 + v0t + .5*a*(t*t));
+		
+		System.out.println(s);
 
 	}
 
