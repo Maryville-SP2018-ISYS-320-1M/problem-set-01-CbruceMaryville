149377a218a72108fe70a31100ad3b9925db908d
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-04-01 19:26:36 -0500

    Update P9_ATMDispenser.java

--- src/P9_ATMDispenser.java
+++ src/P9_ATMDispenser.java
@@ -1,14 +1,22 @@
 /*
 	ISYS 320
-	Name(s):
-	Date: 
+	Name(s):Clayton Bruce
+	Date: 4/1/2018
 */
 
 public class P9_ATMDispenser {
 
 	public static void main(String[] args) {
-		
+		GiveTens(85);
 	}
 
+	public static void GiveTens(int money) {
+		int tens;
+		System.out.println("Here is :" +money%10 + " one dollar bills");
+		tens = money - (money%10);
+		System.out.println("Here is :"+ tens/10 +" ten dollar bills");
+	}
+	
+		
 }
 
