ebe9f1c8be9e6524a9f010e8ec079d49be29db65
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-05-06 20:01:31 -0500

    Update P5_DescendingSquares.java

--- src/P5_DescendingSquares.java
+++ src/P5_DescendingSquares.java
@@ -2,11 +2,14 @@
   	ISYS 320
   	Name(s):Clayton Bruce
   	Date: 4/1/2018
+	EDITED ON 5/6/2018
 */
 
 /* 6. Your pseudocode algorithm here
-
-
+	for i = 10 while not 0 i -- {
+	Now compute i^2 or i*i
+	print(result sepearated by a space)
+	}
 */
 
 public class P5_DescendingSquares {
